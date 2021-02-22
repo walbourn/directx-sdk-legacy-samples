fb76772cd918a493098f5ae33fb88bab57b4e782
Chuck Walbourn <chuckw@windows.microsoft.com>
2021-02-22 02:59:27 -0800

    basic DXUT sampls

---
+++ Direct3D11/EmptyProject11/EmptyProject9.cpp
@@ -0,0 +1,82 @@
+//--------------------------------------------------------------------------------------
+// File: EmptyProject9.cpp
+//
+// Empty starting point for new Direct3D 9 and/or Direct3D 11 applications
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+#include "DXUT.h"
+
+
+//--------------------------------------------------------------------------------------
+// Rejects any D3D9 devices that aren't acceptable to the app by returning false
+//--------------------------------------------------------------------------------------
+bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
+                                      bool bWindowed, void* pUserContext )
+{
+    // Typically want to skip back buffer formats that don't support alpha blending
+    IDirect3D9* pD3D = DXUTGetD3D9Object();
+    if( FAILED( pD3D->CheckDeviceFormat( pCaps->AdapterOrdinal, pCaps->DeviceType,
+                                         AdapterFormat, D3DUSAGE_QUERY_POSTPIXELSHADER_BLENDING,
+                                         D3DRTYPE_TEXTURE, BackBufferFormat ) ) )
+        return false;
+
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that will live through a device reset (D3DPOOL_MANAGED)
+// and aren't tied to the back buffer size
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                     void* pUserContext )
+{
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that won't live through a device reset (D3DPOOL_DEFAULT) 
+// or that are tied to the back buffer size 
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                    void* pUserContext )
+{
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the scene using the D3D9 device
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime, void* pUserContext )
+{
+    HRESULT hr;
+
+    // Clear the render target and the zbuffer 
+    V( pd3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_ARGB( 0, 45, 50, 170 ), 1.0f, 0 ) );
+
+    // Render the scene
+    if( SUCCEEDED( pd3dDevice->BeginScene() ) )
+    {
+        V( pd3dDevice->EndScene() );
+    }
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9ResetDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9LostDevice( void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9CreateDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9DestroyDevice( void* pUserContext )
+{
+}
+
